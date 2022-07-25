7dae1f690d569170f193e49f5cdf2f2f55caf682
arturkamimura <58118744+arturkamimura@users.noreply.github.com>
2022-07-25 15:25:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from flask import Flask, render_template, request, jsonify,  send_file, url_for,  session, redirect
-import os, json, copy, statistics
-from json import dumps
-import pandas as pd
-import io
-from werkzeug.utils import secure_filename
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,7 +10,6 @@ def index():
 
 if __name__ == '__main__':
 
-    app.config['SESSION_TYPE'] = 'filesystem'
 
     app.run(debug=True, host='0.0.0.0', port=5000)
 
